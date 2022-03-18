5ea0ea746a9be35e6c6d8398e5e661a1551ec6a6
Tennana <tennana@tennana.net>
2022-03-19 00:22:01 +0900

    fix: TwitterカードのURLが出力されない

--- _data/site.js
+++ _data/site.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "奈良県奈良市の冒険企画局オンリーTRPGコンベンション「Ebicon」の告知サイトです。",
     summary: "奈良県奈良市のTRPGコンベンション「Ebicon」の告知サイト記事です。",
     lang: "ja",
-    siteUrl: "https://ebicon.tennana.net/",
+    siteUrl: "https://ebicon.tennana.net",
   },
   feed: { // used in feed.xml.njk
     subtitle: "Lorem ipsum dolor sit amet consecuteor",
